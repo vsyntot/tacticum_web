6c3f6d5508a81eb16f63d32af9f88937c2e00bf0
vsyntot <55494540+vsyntot@users.noreply.github.com>
2026-01-17 23:16:49 +0300

    Add shared menu tree helper

--- local/templates/tacticum/components/bitrix/menu/topmenu/result_modifier.php
+++ local/templates/tacticum/components/bitrix/menu/topmenu/result_modifier.php
@@ -1,5 +1,9 @@
 <?php
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/include/menu_helpers.php';
 
 // Собираем список главных пунктов меню
 $mainMenuTitles = [];
@@ -7,42 +11,6 @@ foreach ($arResult as $item) {
     if ($item['DEPTH_LEVEL'] == 1) {
         $mainMenuTitles[] = $item['TEXT'];
     }
-}
-
-// Функция для построения дерева меню
-function buildMenuTree($menu, $mainMenuTitles) {
-    $tree = [];
-    $parents = [];
-
-    foreach ($menu as $key => &$item) {
-        $item['CHILDREN'] = [];
-        $parents[$item['DEPTH_LEVEL']][$key] = &$item;
-
-        if ($item['DEPTH_LEVEL'] == 1) {
-            $tree[$key] = &$item;
-        } else {
-            $parentLevel = $item['DEPTH_LEVEL'] - 1;
-            end($parents[$parentLevel]);
-            $parentKey = key($parents[$parentLevel]);
-            if (isset($parents[$parentLevel][$parentKey])) {
-                $parents[$parentLevel][$parentKey]['CHILDREN'][] = &$item;
-            }
-        }
-    }
-
-    // Фильтруем дубли у каждого CHILDREN сразу
-    foreach ($tree as &$node) {
-        if (!empty($node['CHILDREN'])) {
-            $node['CHILDREN'] = array_filter(
-                $node['CHILDREN'],
-                function($child) use ($mainMenuTitles) {
-                    return !in_array($child["TEXT"], $mainMenuTitles);
-                }
-            );
-        }
-    }
-
-    return $tree;
 }
 
 $arResult['MENU_TREE'] = buildMenuTree($arResult, $mainMenuTitles);
